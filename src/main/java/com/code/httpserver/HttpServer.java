a61ff72c9a1499f77240039821568753b6a9b6a0
Alexandru Contra <contraalex8@gmail.com>
2021-10-25 15:33:34 +0300

    managed to start server and proceed the configurations from the json file

--- src/main/java/com/code/httpserver/HttpServer.java
+++ src/main/java/com/code/httpserver/HttpServer.java
@@ -1,10 +1,17 @@
 package com.code.httpserver;
 
+import com.code.httpserver.config.Configuration;
+import com.code.httpserver.config.ConfigurationManager;
+
 /*
 We'll consider this class as  a driver class
 */
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("SERVER STARTING...");
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration config = ConfigurationManager.getInstance().getCurrentConfiguration();
+        System.out.println("USING PORT: "+config.getPort());
+        System.out.print("USING WEBROOT:"+config.getWebroot());
     }
 }

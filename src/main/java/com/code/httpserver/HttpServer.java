16e515f7fb83570f0a248640a4e245ce8a1562bf
Alexandru Contra <contraalex8@gmail.com>
2021-10-25 15:43:25 +0300

    working server

--- src/main/java/com/code/httpserver/HttpServer.java
+++ src/main/java/com/code/httpserver/HttpServer.java
@@ -3,6 +3,12 @@ package com.code.httpserver;
 import com.code.httpserver.config.Configuration;
 import com.code.httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /*
 We'll consider this class as  a driver class
 */
@@ -13,5 +19,25 @@ public class HttpServer {
         Configuration config = ConfigurationManager.getInstance().getCurrentConfiguration();
         System.out.println("USING PORT: "+config.getPort());
         System.out.print("USING WEBROOT:"+config.getWebroot());
+
+        try {
+            ServerSocket serverSocket  = new ServerSocket(config.getPort());
+            //prompt the socket and accept any connections
+            Socket socket =   serverSocket.accept();
+           InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+            //TODO PROCEED READING
+            //writing
+            String html = "<html><head><title>Simple JAVA HTTTP SERVER</title></head><body><h1>CONTENT OF HTTP SERVER</h1></body></html>";
+            final String CRLF = "\n\r"; //13 ,10
+            String response = "HTTP/1.1 200 OK"+CRLF + "CONTENT-LENGHT: "+html.getBytes().length+CRLF + CRLF+html+CRLF;//Status line : HTTP  VERSION-> RESPONSE -> CODE -> RESPONSE_MESSAGE
+            outputStream.write(response.getBytes());
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
